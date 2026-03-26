1df7da71e8d0984631fbb783f90f576e7d8be13e
Prenoshan <prenoshan.kn@gmail.com>
2026-03-26 18:48:58 +0200

    Connected to date.nager API

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def main():
-    print("Hello World")
+import requests
 
-if __name__ == "__main__":
-    main()
+# URL of the API
+url = "https://date.nager.at/api/v3/PublicHolidays/2026/za"
+
+# Make a GET request
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse JSON response
+    data = response.json()
+
+    for holidays in data:
+        print(holidays["date"], holidays["name"])
+else:
+    print(f"Error: {response.status_code}")
